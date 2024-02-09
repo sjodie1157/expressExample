2099dcd91347c928ff5908f4c2a8a1b5676e686f
sjodie1157 <sjodie1157@gmail.com>
2024-02-09 16:16:52 +0200

    added code

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log(`Welcome to Expreess JS`);
+import express from 'express'
+
+const port = +process.env.PORT || 4000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: req.statusCode,
+        msg: `You're Home`
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).json({
+        status: req.statusCode,
+        msg: `About page`
+    })
+})
+
+app.get('*', (req, res) => {
+    res.status(200).json({
+        status: 404,
+        msg: `404 page`
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server is on : http://localhost:${port}`)
+})
